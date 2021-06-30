bf01a280817cc3eb4df2a69e68d3f42896f2d19c
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-06-30 18:01:44 +0100

    add select_homoplasy.R script

---
+++ select_homoplasy.R
@@ -0,0 +1,20 @@
+data<-read.table("output.homoplasies.txt")
+
+min_big_cluster_size<-5
+min_other_occurences<-2
+min_other_samples<-10
+
+homoplastic_test<-tapply(data$V3, data$V1,function(x){
+  if (
+    (max(x) >= min_big_cluster_size) &&
+    (length(x)-1 >= min_other_occurences) &&
+    (sum(x)-max(x) >= min_other_samples)
+    ){
+      TRUE
+    } else {
+      FALSE
+    }
+})
+
+homoplasic_pos<-as.numeric(names(homoplastic_test[which(homoplastic_test)]))
+
